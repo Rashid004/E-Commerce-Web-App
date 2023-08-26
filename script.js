f07f38cda374608b3b93e4b50bfe9eb5f3b29a4b
Your Name <you@example.com>
2023-08-26 16:54:34 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use-strict'
+
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function(entries) {
+ const [entry] = entries;
+
+ if(!entry.isIntersecting)
+ nav.classList.add('sticky');
+ else 
+  nav.classList.remove('sticky');
+}
+
+
+const headerObsever = new IntersectionObserver(stickyNav,{
+ root:null,
+ threshold:0,
+ rootMargin: `${navHeight}px`,
+});
+headerObsever.observe(header);
+
+
+
+
+
+
+
+// const navHeight = nav.getBoundingClientRect().height;
+// const stickyNav = function (entries) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// };
+
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: `-${navHeight}px`,
+// });
+// headerObserver.observe(header);
