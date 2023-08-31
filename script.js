22703cb53ce9466d0460a5dc43a32429c055b4ce
Your Name <you@example.com>
2023-08-31 19:28:55 +0530

    Add Product Cart

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ const nav = document.querySelector('.nav');
 const navBar = document.querySelector('.nav-bar');
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
+const pro = document.querySelector('.pro');
 
-
+// For mobile responsive
+const hamBurger = () => {
   bar.addEventListener('click',() => {
     navBar.style.transform = 'translateX(-0px)';
   });
-
   close.addEventListener('click',() => {
     navBar.style.transform = 'translateX(300px)';
   });
+};
+hamBurger();
 
-
+pro.onclick = () => {
+  window.location.href = "./product.html";
+};
+  
 
 
 // Sticky Nav
@@ -38,22 +44,27 @@ const headerObsever = new IntersectionObserver(stickyNav,{
 headerObsever.observe(header);
 
 
+// Carousel (Slider) in product details
 
+const smallImg = document.querySelectorAll(".small-img");
+const mainImg = document.querySelector(".main-img");
 
+// Old Method
 
-
-
-// const navHeight = nav.getBoundingClientRect().height;
-// const stickyNav = function (entries) {
-//   const [entry] = entries;
-
-//   if (!entry.isIntersecting) nav.classList.add("sticky");
-//   else nav.classList.remove("sticky");
-// };
-
-// const headerObserver = new IntersectionObserver(stickyNav, {
-//   root: null,
-//   threshold: 0,
-//   rootMargin: `-${navHeight}px`,
-// });
-// headerObserver.observe(header);
+// smallImg[0].onclick = function() {
+//   mainImg.src = smallImg[0].src
+// }
+// smallImg[1].onclick = function() {
+//   mainImg.src = smallImg[1].src
+// }
+// smallImg[2].onclick = function() {
+//   mainImg.src = smallImg[2].src
+// }
+// smallImg[3].onclick = function() {
+//   mainImg.src = smallImg[3].src
+// }
+smallImg.forEach((img, index) => {
+  img.onclick = () => {
+    mainImg.src = img.src;
+  };
+});
