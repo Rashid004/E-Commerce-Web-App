ebfa57b04380d135008982e9d1f508c96fbcd387
Your Name <you@example.com>
2023-08-31 07:49:54 +0530

    Media query

--- script.js
+++ script.js
@@ -2,7 +2,23 @@
 
 const header = document.querySelector('.header');
 const nav = document.querySelector('.nav');
+const navBar = document.querySelector('.nav-bar');
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
 
+
+  bar.addEventListener('click',() => {
+    navBar.style.transform = 'translateX(-0px)';
+  });
+
+  close.addEventListener('click',() => {
+    navBar.style.transform = 'translateX(300px)';
+  });
+
+
+
+
+// Sticky Nav
 const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function(entries) {
  const [entry] = entries;
